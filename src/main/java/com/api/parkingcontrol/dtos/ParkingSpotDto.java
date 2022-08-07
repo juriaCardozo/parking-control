f963450fcb6ea397ec6147a49f297f02dc2452ad
juriaCardozo <ccardozojulia@gmail.com>
2022-08-07 00:00:24 -0300

    Implementing ParkingSpotDto

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -1,5 +1,24 @@
 package com.api.parkingcontrol.dtos;
 
-public class ParkingSpotDto {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+public class ParkingSpotDto {
+    @NotBlank
+    private String parkingSpotNumber;
+    @NotBlank
+    @Size(max = 7)
+    private String licencePlateCar;
+    @NotBlank
+    private String brandCar;
+    @NotBlank
+    private String modelCar;
+    @NotBlank
+    private String colorCar;
+    @NotBlank
+    private String responsibleName;
+    @NotBlank
+    private String apartment;
+    @NotBlank
+    private String block;
 }
