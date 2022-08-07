f4b868dfa5328061b15beef21f602e11c07811d5
juriaCardozo <ccardozojulia@gmail.com>
2022-08-07 00:42:52 -0300

    Implementing Post method

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -21,4 +21,68 @@ public class ParkingSpotDto {
     private String apartment;
     @NotBlank
     private String block;
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicencePlateCar() {
+        return licencePlateCar;
+    }
+
+    public void setLicencePlateCar(String licencePlateCar) {
+        this.licencePlateCar = licencePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
