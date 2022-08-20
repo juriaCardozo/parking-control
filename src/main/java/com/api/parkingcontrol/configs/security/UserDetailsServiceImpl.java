3fe43957419dddfebc8cfb269b7005b565b6e011
Júlia Cardozo <ccardozojulia@gmail.com>
2022-08-20 13:50:12 -0300

    Implementing UserDetailsServiceImpl

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -1,5 +1,23 @@
 package com.api.parkingcontrol.configs.security;
 
-public class UserDetailsServiceImpl {
+import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
