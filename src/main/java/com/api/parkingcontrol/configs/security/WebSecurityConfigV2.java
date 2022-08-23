3dcc2faad8878ec0f9c706c11a857e0bd55faa58
Júlia Cardozo <ccardozojulia@gmail.com>
2022-08-23 12:12:44 -0300

    Configurando Spring Security sem WebSecurityConfigurerAdapter

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigV2.java
@@ -3,24 +3,16 @@ package com.api.parkingcontrol.configs.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
+public class WebSecurityConfigV2 {
 
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    final UserDetailsServiceImpl userDetailsService;
-
-    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
@@ -31,16 +23,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+        return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
